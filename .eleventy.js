d6a054d6ae32cb6f93d55270f9c3004df7dd01bc
gianni <gianni.crivello96@gmail.com>
2022-05-28 03:36:46 -0700

    cleaned up project and progressed in using 11ty shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,42 @@
+const {html} = require('common-tags');
+
+
 module.exports = function(config) {
 	
 
 	config.addPassthroughCopy('src/styles');
 	config.addPassthroughCopy('src/js');
 	config.addPassthroughCopy('src/public');
+
+	config.addShortcode("socials", function(link, img, description) {
+		console.log(link);
+		console.log(img);
+		console.log(description);
+		if(link != ""){
+			return html`
+			<a href=${link}>
+			<img src=${img} alt=${description} width="120" height="120" />
+			</a>
+			`;
+		} else {
+			return html`
+			<a href=${defaultLink}>
+			<img src=${defaultSrc} alt=${defaultAlt} width="120" height="120" />
+			</a>
+			`;
+		}
+	})
+
+
+
+
+
+
+
 	return {
 		dir: {
 			input: "src",
-			output: "dist"
+			output: "docs"
 		}
 	}
 }
